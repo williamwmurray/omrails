bbe684d41d877a52619969cfde047814c9705c2c
williamwmurray <williamwmurray@gmail.com>
2014-01-26 19:23:56 +0000

    Initial push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pint::Application.config.secret_key_base = '633e4c07d720591e80bdb8c2933c6ed78da058ae088113c78f6794008ee34dad9b79d79902f658dbdfb5bfe45a0ae269c9aa657116301c92e374afc926c4f410'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pint::Application.config.session_store :cookie_store, key: '_pint_session'
